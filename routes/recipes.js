a740bb7c17c175758f58ec2f7471b1f9b1f164b1
Taras-B <taras5475@gmail.com>
2020-08-19 18:06:38 +0300

    update recipe router add

--- routes/recipes.js
+++ routes/recipes.js
@@ -8,6 +8,7 @@ router.get('/', async (req, res) => {
     res.json({ recipes: recipes })
   } catch (e) {
     console.log(e)
+    res.json({ message: 'Щось пішло не так' })
   }
 })
 
@@ -18,17 +19,34 @@ router.post('/add', async (req, res) => {
     const recipe = new Recipe({ title, description })
 
     await recipe.save()
-    res.json({ msg: 'Рецепт був доданий' })
+    res.json({ message: 'Рецепт був доданий' })
   } catch (e) {
     console.log(e)
-    res.json({ msg: 'Щось пішло не так' })
+    res.json({ message: 'Щось пішло не так' })
+  }
+})
+
+router.post('/edit/:id', async (req, res) => {
+  try {
+    await Recipe.findByIdAndUpdate(req.params.id, req.body, (err) => {
+      if (err)
+        return res
+          .status(500)
+          .json({ success: false, message: 'There was a problem updating the recipe.' })
+      res.json({ success: true, message: 'Recipe updated.' })
+    })
+  } catch (e) {
+    console.log(e)
   }
 })
 
 router.delete('/delete/:id', async (req, res) => {
   try {
-    Recipe.findByIdAndRemove(req.params.id, (err) => {
-      if (err) return res.status(500).send('There was a problem deleting the user.')
+    await Recipe.findByIdAndRemove(req.params.id, (err) => {
+      if (err)
+        return res
+          .status(500)
+          .json({ success: false, message: 'There was a problem deleting the recipe.' })
       res.json({ success: true, message: 'Recipe deleted.' })
     })
   } catch (e) {
