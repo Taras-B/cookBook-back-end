3b45630ee4730a073f8c4c3f9e843f7e3a02112b
Taras-B <taras5475@gmail.com>
2020-09-21 17:27:20 +0300

    heroku 3

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 const passport = require('passport')
 
 const bodyParser = require('body-parser')
